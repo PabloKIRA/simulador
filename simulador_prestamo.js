05e4cfce47d4a5e5f1d931731d98c33ad554a79f
PabloKIRA <126306308+PabloKIRA@users.noreply.github.com>
2024-05-29 21:20:20 -0300

    toasty

--- simulador_prestamo.js
+++ simulador_prestamo.js
@@ -27,9 +27,34 @@ function calcularYMostrar() {
 
 // Función para mostrar el resultado en un cuadro emergente
 function mostrarResultado(pagoMensual) {
-    alert(`El pago mensual será de: $${pagoMensual}`);
+    const cuotasInput = document.getElementById('cuotas');
+    cuotasInput.value = pagoMensual;
+    Toastify({
+        text: `El pago mensual será de: $${pagoMensual}`,
+        duration: 5000, // Duración de la notificación en milisegundos
+        close: true,    // Mostrar botón de cierre
+        gravity: "top", // Posición de la notificación
+        position: "right", // Alineación de la notificación
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+        stopOnFocus: true, // Detener el temporizador cuando se pase el ratón sobre la notificación
+    }).showToast();
 }
 
 // Agregar el evento de clic al botón "Calcular Pago Mensual"
 const calculateButton = document.getElementById('calculateButton');
 calculateButton.addEventListener('click', calcularYMostrar);
+Toastify({
+    text: "Conoce las cotizaciones de todas las divisas online",
+    duration: 5000,
+    destination: "https://es.investing.com/currencies/://example.com",
+    newWindow: true, // Abrir el enlace en una nueva ventana
+    close: true,
+    gravity: "top", 
+    position: "left",
+    backgroundColor:
+        "linear-gradient(to right, #00b09b, #96c93d)",
+    stopOnFocus: true, // Mantener el toast visible mientras el mouse esté sobre él
+    onClick: function() {
+        console.log("Toast clicked!");
+    }
+}).showToast();
